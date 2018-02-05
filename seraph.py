a9e876d21ee0beb7daca5408a1102d2879b377a4
P B Austin <pbaustin96@gmail.com>
2018-02-05 10:46:45 +0530

    Create seraph.py
    For karunya seraph login. Posted at https://forums.karunya.edu/t/how-to-directly-get-to-seraph-login-page/2741

---
+++ seraph.py
@@ -0,0 +1,26 @@
+import requests
+
+
+username="UR14CS100" 
+password="PASSWORD123" 
+
+
+sess = requests.Session()
+
+#To get the seraph page. We need the magic number to authenticate. 
+#Without the magic number it wont work
+a= sess.get('http://go.microsoft.com')
+
+content = str(a.content)
+try:
+	magic = content.index('magic')
+	magicVal=content[magic:magic+40].split('"')[2] #located the magic number
+except :
+		print("Already authenticated")	
+		exit(0)
+		
+#Send the post with magic.
+auth=requests.post("https://seraph.karunya.edu:1003", data={"username":username,"4Tredir":"http%3A%2F%2Fgo.microsoft.com%2F","magic":magicVal,"password":password })
+print("Seraph Authenticated")
+exit(0)
+
