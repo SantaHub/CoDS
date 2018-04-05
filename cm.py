ad07d9472a97f9c88e3b5ba5698cff58aff2c442
P B Austin <pbaustin96@gmail.com>
2018-04-05 17:08:53 +0530

    Confusion Matrix

---
+++ cm.py
@@ -0,0 +1,29 @@
+def plot_cm(clf, labels = ['rovnix', 'None', 'conficker', 'murofet', 'ramdo', 'tinba']):
+    cm  = confusion_matrix(y, clf['y_pred'])
+    percent = (cm*100.0)/np.array(np.matrix(cm.sum(axis=1)).T) 
+    print ('\nConfusion Matrix Stats : ',clf['name'],clf['acc'],'%')
+    for i, label_i in enumerate(labels):
+        for j, label_j in enumerate(labels):
+            print ("%s/%s: %.2f%% (%d/%d)" % (label_i, label_j, (percent[i][j]), cm[i][j], cm[i].sum()))
+            
+    fig = plt.figure(figsize=(20,10))
+    ax = fig.add_subplot(111)
+    ax.grid(b=False)
+    cax = ax.matshow(percent, cmap=plt.cm.Blues)
+    pylab.title('Confusion matrix of the classifier : '+ clf['name']+'\n')
+    fig.colorbar(cax)
+    ax.set_xticklabels([' '] + labels)
+    ax.set_yticklabels([' '] + labels)
+#    ax.text(0,0,percent[0][0],va='center',ha='center',bbox=dict(fc='w',boxstyle='round,pad=1'))
+#    ax.text(0,1,percent[0][1],va='center',ha='center',bbox=dict(fc='w',boxstyle='round,pad=1'))
+#    ax.text(1,0,percent[1][0],va='center',ha='center',bbox=dict(fc='w',boxstyle='round,pad=1'))
+#    ax.text(1,1,percent[1][1],va='center',ha='center',bbox=dict(fc='w',boxstyle='round,pad=1'))
+    for x,Y in enumerate(percent):
+        for a,b in enumerate(Y):
+            ax.text(x,a,b,va='center',ha='center',bbox=dict(fc='w',boxstyle='round,pad=1'))
+            
+    pylab.xlabel('Predicted')
+    pylab.ylabel('Actual')
+    pylab.savefig('./ClassifierImages/multi/'+clf['name']+'.png')
+    pylab.show()
+    
